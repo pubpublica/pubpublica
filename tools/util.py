1a70359df358f5dc3a2ef0c00605a34d8295ea42
Jens Christian Jensen <jensecj@gmail.com>
2019-10-17 00:41:45 +0200

    util.py: added fns to get timestamp and prettyprint json

--- tools/util.py
+++ tools/util.py
@@ -9,6 +9,10 @@ from jinja2 import Template
 import log
 
 
+def timestamp():
+    return datetime.datetime.utcnow().isoformat()
+
+
 def template(file, config={}):
     contents = ""
     with open(file, "r") as f:
@@ -34,15 +38,21 @@ def connect(host, sudo=False):
     return c
 
 
+def print_json(j):
+    print(json.dumps(j, indent=4))
+
+
 class Guard:
     def __init__(self, str):
         self.str = str
 
     def __enter__(self):
         sys.stdout.write(self.str)
+        sys.stdout.flush()
 
     def __exit__(self, type, value, traceback):
         if not (type and value and traceback):
             log.success("DONE")
         else:
             log.error("FAILED")
+        sys.stdout.flush()
