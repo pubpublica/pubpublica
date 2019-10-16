635457b16fa9ab415d69e33a46ea0bd2a8035f19
Jens Christian Jensen <jensecj@gmail.com>
2019-10-16 12:48:29 +0200

    more config loading updates, added default context to routes

--- pubpublica/app.py
+++ pubpublica/app.py
@@ -6,27 +6,34 @@ from flask import Flask, render_template, make_response
 from flask import request
 from flask_caching import Cache
 
-from pubpublica import utils
+from pubpublica import util
+
+from config import config
 
 app = Flask(__name__)
-app.config.update(utils.load_json(".pubpublica"))
-app.config.update(utils.load_json(".flask"))
-
-print(app.config)
+app.config.update(util.load_json(".flask"))
+app.config.update({"pubpublica": util.load_json(".pubpublica")})
 
 cache_config = {}
-cache_config.update(utils.load_json(".redis"))
-
+cache_config.update(util.load_json(".redis"))
 cache = Cache(config=cache_config)
 cache.init_app(app)
+
+def default_context():
+    return {"commit": "", "timestamp": ""}
 
 
 @app.route("/")
 @cache.cached(timeout=60)
 def index():
-    ctx = app.config.get("pubpublica", {})
+    ctx = default_context()
+    ctx.update(app.config.get("pubpublica", {}))
 
-    pubs = utils.get_publications(ctx.get("PUBLICATIONS_PATH", {}))
+    pubs = []
+    path = app.config.get("PUBLICATIONS_PATH")
+    if path:
+        pubs = util.get_publications(path)
+
     ctx.update({"publications": pubs})
 
     return render_template("index.html", ctx=ctx)
@@ -35,10 +42,15 @@ def index():
 @app.route("/rss")
 @cache.cached(timeout=500)
 def rss():
+    ctx = default_context()
     ctx = app.config.get("pubpublica", {})
 
-    pubs = utils.get_publications(ctx.get("PUBLICATIONS_PATH", {}))
-    pubs = utils.rss_convert(pubs)
+    pubs = []
+    path = app.config.get("PUBLICATIONS_PATH")
+    if path:
+        pubs = util.get_publications(path)
+
+    pubs = util.rss_convert(pubs)
 
     ctx.update({"publications": pubs})
 
