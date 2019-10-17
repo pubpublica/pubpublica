d7513186ab4b6efae195bcc17ac502437f18bd6e
Jens Christian Jensen <jensecj@gmail.com>
2019-10-17 11:42:16 +0200

    log.py: use bold attributes, improves legibility

--- tools/log.py
+++ tools/log.py
@@ -3,12 +3,12 @@ from termcolor import colored
 
 
 def success(s):
-    print(colored(s, "green"))
+    print(colored(s, "green", attrs=["bold"]))
 
 
 def warning(s):
-    print(colored(s, "yellow"))
+    print(colored(s, "yellow", attrs=["bold"]))
 
 
 def error(s):
-    print(colored(s, "red"), file=sys.stderr)
+    print(colored(s, "red", attrs=["bold"]), file=sys.stderr)
