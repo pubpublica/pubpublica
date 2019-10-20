a5306f052de50e7d2308e263e6fdff26e3ba6bda
Jens Christian Jensen <jensecj@gmail.com>
2019-10-20 12:41:15 +0200

    updated config.py structure

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 config = {
-    "BUILD": {
-        "LOCAL_CONFIG_PATH": "config/",
+    "BUILD": {"LOCAL_CONFIG_PATH": "config/"},
+    "PROVISION": {
         "DEPENDENCIES": [
             "python3",
             "python3-pip",
             "python3-venv",
             "redis-server",
             "nginx",
-        ],
+        ]
+    },
+    "DEPLOY": {
+        "USER": "pubpublica",
+        "GROUP": "pubpublica",
+        "APP_PATH": "pubpublica/",
         "INCLUDES": [
             "__version__.py",
             "requirements.txt",
@@ -17,11 +22,16 @@ config = {
             "pubpublica/",
         ],
     },
-    "PROVISION": {},
-    "DEPLOY": {"USER": "pubpublica", "GROUP": "pubpublica", "APP_PATH": "pubpublica/"},
-    "PUBPUBLICA": {"PUBLICATIONS_PATH": "publications/"},
-    "FLASK": {"FLASK_SECRET_KEY_PATH": "cayenne/flask/key"},
+    "PUBPUBLICA": {
+        "PUBPUBLICA_CONFIG_FILE": ".pubpublica",
+        "PUBLICATIONS_PATH": "publications/",
+    },
+    "FLASK": {
+        "FLASK_CONFIG_FILE": ".flask",
+        "FLASK_SECRET_KEY_PATH": "cayenne/flask/key",
+    },
     "REDIS": {
+        "REDIS_CONFIG_FILE": ".redis",
         "REDIS_HOST": "localhost",
         "REDIS_PORT": 6379,
         "REDIS_PASSWORD_PATH": "cayenne/redis/key",
