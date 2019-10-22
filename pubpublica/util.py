1f812ad4bb68137ebd44f8f564d6683741388113
Jens Christian Jensen <jensecj@gmail.com>
2019-10-22 23:12:52 +0200

    use the logging library in util.py

--- pubpublica/util.py
+++ pubpublica/util.py
@@ -1,6 +1,9 @@
 import os
 import json
 import datetime
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def load_json(file):
@@ -8,8 +11,7 @@ def load_json(file):
         with open(file, "r") as f:
             return json.load(f)
     except Exception as err:
-        print(f"failed to load json from {file}")
-        print(err)
+        log.warning(f"failed to load JSON from {file}")
         return {}
 
 
@@ -19,7 +21,9 @@ def get_publication_files(path):
 
     files = []
     for _, _, fs in os.walk(pubdir):
+        log.debug("publication files:")
         for f in fs:
+            log.debug(f)
             files.append(os.path.join(pubdir, f))
 
     return files
@@ -27,25 +31,24 @@ def get_publication_files(path):
 
 def validate_pub_file(file):
     if not os.path.exists(file):
-        print("invalid pub file: does not exist")
+        log.error("invalid pub file: does not exist")
         return False
 
     if not os.path.isfile(file):
-        print("invalid pub file: is not a file")
+        log.error("invalid pub file: is not a file")
         return False
 
     if not file.endswith(".json"):
-        print("invalid pub file: is not a json file")
+        log.error("invalid pub file: is not a json file")
         return False
 
-    j = {}
     with open(file, "r") as f:
         j = json.load(f)
 
     if not (
         j.get("title") and j.get("date") and j.get("authors") and j.get("description")
     ):
-        print("invalid pub file: invalid structure")
+        log.error("invalid pub file: invalid structure")
         return False
 
     return True
@@ -55,15 +58,16 @@ def load_pub(file):
     if not validate_pub_file(file):
         return None
 
-    pub = {}
     with open(file, "r") as f:
         return json.load(f)
 
 
 def get_publications(path):
     files = get_publication_files(path)
+
     pubs = [load_pub(f) for f in files]
     pubs = [p for p in pubs if p]
+
     return pubs
 
 
