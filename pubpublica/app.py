3a470972722bc5e7f9b2af913511732e569d7186
Jens Christian Jensen <jensecj@gmail.com>
2019-10-22 23:14:39 +0200

    setup logging in app.py, configures root + flask logger

--- pubpublica/app.py
+++ pubpublica/app.py
@@ -1,27 +1,54 @@
 import os
 import json
 import random
+import logging
+
+from systemd import journal
 
 from flask import Flask, render_template, make_response
 from flask import request
+from flask.logging import default_handler
 from flask_caching import Cache
 
 from pubpublica import util
+
+# we setup logging before creating the flask app so it uses this
+# logging configuration.
+
+# we configure the root log so other modules will also use this
+# configuration.
+
+root_log = logging.getLogger()
+root_log.setLevel(logging.DEBUG)
+
+log_format = "%(asctime)s - [%(levelname)s]: %(message)s " ""
+formatter = logging.Formatter(log_format, datefmt="%Y-%m-%d %H:%M:%S")
+
+# the stream handler pipes the output to stdout/stderr, which is in
+# turn handles by uwsgi, and put into a corresponding rotating logfile
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+# also log things to the systemd journal
+journald_handler = journal.JournalHandler(SYSLOG_IDENTIFIER="pubpublica")
+
+root_log.addHandler(stream_handler)
+root_log.addHandler(journald_handler)
 
 app = Flask(__name__)
 
 flask_config = util.load_json(".flask")
 if flask_config:
-    print("loaded flask config from .flask")
+    app.logger.info("loaded flask config from .flask")
     app.config.update(flask_config)
 
 pubpublica_config = util.load_json(".pubpublica")
 if pubpublica_config:
-    print("loaded pubpublica config from .pubpublica")
+    app.logger.info("loaded pubpublica config from .pubpublica")
 
 cache_config = util.load_json(".redis")
 if cache_config:
-    print("loaded cache config from .redis")
+    app.logger.info("loaded cache config from .redis")
 
 cache = Cache(config=cache_config)
 cache.init_app(app)
@@ -30,6 +57,8 @@ cache.init_app(app)
 @app.route("/")
 @cache.cached(timeout=300)
 def index():
+    app.logger.info("Generating INDEX for cache.")
+
     pubs = []
     path = pubpublica_config.get("PUBLICATIONS_PATH")
     if path:
